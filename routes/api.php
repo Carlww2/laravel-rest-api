6f30cdd6ea30f8c2b56efedeae23d52d433813c9
Luis David Castaneda Rodriguez <luis.castaneda@office365.3pillarglobal.com>
2023-01-26 16:04:58 -0600

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/reset', 'Resetcontroller@reset');
+Route::get('/balance', 'BalanceController@show');
+Route::post('/event', 'EventController@store');
